0e24b90b710a6c2525b6c8ae52b93cb96862e9e7
FC243401 <pl1627446@gmail.com>
2026-02-27 15:55:01 +0600

    Update BinSearch.cpp

--- BinSearch.cpp
+++ BinSearch.cpp
@@ -1,36 +1,46 @@
 #include <iostream>
 using namespace std;
-int main() {
-int N;
-cout << "Enter number of elements: ";
-cin >> N;
-int data[100];
-cout << "Enter sorted elements: ";
-for (int i = 0; i < N; i++) {
-cin >> data[i];
-}
-int item;
-cout << "Enter item to search: ";
-cin >> item;
 
-int BEG = 0;
-int END = N - 1;
-int MID;
-int LOC = 0;
-while (BEG <= END) {
-        MID = (BEG + END) / 2;
-        if (data[MID] == item) {
-            LOC = MID + 1;
+int main() {
+    int n, target;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter sorted elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter target value: ";
+    cin >> target;
+
+    int left = 0;
+    int right = n - 1;
+    int mid;
+    int found = -1;
+
+    while (left <= right) {
+        mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            found = mid;
+            break;
         }
-        else if (item < data[MID]) {
-            END = MID - 1;
+        else if (arr[mid] < target) {
+            left = mid + 1;
         }
         else {
-            BEG = MID + 1;}}
-if (LOC == 0)
-        cout << "Item not found." << endl;
+            right = mid - 1;
+        }
+    }
+
+    if (found != -1)
+        cout << "Element found at index: " << found << endl;
     else
-        cout << "Item found at location: " << LOC << endl;
+        cout << "Element not found" << endl;
 
     return 0;
 }
